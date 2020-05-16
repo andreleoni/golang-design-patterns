d4131c6f5d59e9786f392cb1a314ac5177b084d5
André Leoni <andreluizleoni@gmail.com>
2020-05-16 13:10:24 -0300

    Create a new filter with open closed principle

--- open-closed-principle/main.go
+++ open-closed-principle/main.go
@@ -50,6 +50,42 @@ type Product struct {
 	size Size
 }
 
+type Specification interface {
+	IsSatisfied(p *Product) bool
+}
+
+type ColorSpecification struct {
+	color Color
+}
+
+func (c ColorSpecification) IsSatisfied(p *Product) bool {
+	return p.color == c.color
+}
+
+type SizeSpecification struct {
+	size Size
+}
+func (s SizeSpecification) IsSatisfied(p *Product) bool {
+	return p.size == s.size
+}
+
+type BetterFilter struct {
+
+}
+
+func (f *BetterFilter) Filter(products []Product, spec Specification) []*Product {
+	result := make([]*Product, 0)
+
+	for i, v := range products {
+		if spec.IsSatisfied(&v) {
+			result = append(result, &products[i])
+		}
+	}
+
+	return result
+}
+
+
 func main() {
 	apple := Product{"Apple", green, small}
 	tree := Product{"Tree", green, large}
@@ -64,6 +100,21 @@ func main() {
 	}
 
 	for _, v := range f.FilterBySize(products, large) {
+		fmt.Printf("    %s is large ", v.name)
+	}
+
+
+
+	/// New filter
+	fmt.Println("\n\nNew implementation \n")
+	bf := BetterFilter{}
+	greenSpecification := ColorSpecification{green}
+	for _, v := range bf.Filter(products, greenSpecification) {
+		fmt.Printf("    %s is green ", v.name)
+	}
+
+	sizeSpecification := SizeSpecification{large}
+	for _, v := range bf.Filter(products, sizeSpecification) {
 		fmt.Printf("    %s is large ", v.name)
 	}
 }
