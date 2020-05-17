c4685308b7cb53cc99ac49107fd24a8619217b99
André Leoni <andreluizleoni@gmail.com>
2020-05-17 20:44:38 -0300

    Created code for square

--- liskov-substitute-principle/with.go
+++ liskov-substitute-principle/with.go
@@ -29,6 +29,35 @@ func (r *Rectangle) SetHeight(height int) {
 	r.height = height
 }
 
+type Square struct {
+	Rectangle
+}
+
+func NewSquare(size int) *Square {
+	sq := Square{}
+	sq.width = size
+	sq.height = size
+	return &sq
+}
+
+func (s *Square) GetWidth() int {
+	return s.width
+}
+
+func (s *Square) GetHeight() int {
+	return s.height
+}
+
+func (s *Square) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+func (s *Square) SetHeight(height int) {
+	s.width = height
+	s.height = height
+}
+
 func UseIt(sized Sized) {
 	width := sized.GetWidth()
 	sized.SetHeight(10)
@@ -38,9 +67,10 @@ func UseIt(sized Sized) {
 	fmt.Println("Expected an area of ", expectArea, ", but got ", actualArea, "\n")
 }
 
-
-
 func main() {
 	rc := &Rectangle{2,3}
 	UseIt(rc)
+
+	sq := NewSquare(5)
+	UseIt(sq)
 }
